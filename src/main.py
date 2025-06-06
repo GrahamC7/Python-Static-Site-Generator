8d6d1734b5ed39ae3290a4d7e414c443ad713047
GrahamC7 <scocker@wgu.edu>
2025-06-06 13:23:57 -0400

    adding copy static functionality to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+            print(f"Copied file: {source_path} -> {dest_path}")
+        elif os.path.isdir(source_path):
+            os.mkdir(dest_path)
+            print("Created directory:, {dest_path}")
+            copy_static(source_path, dest_path)
 
 def main():
+    print("Copying static files...")
+    copy_static("static", "public")
+    print("Done.")
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
 
