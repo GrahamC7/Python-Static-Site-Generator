0011fd869d630178a80f24782e029b731b1808b9
GrahamC7 <scocker@wgu.edu>
2025-06-07 13:19:52 -0400

    updating main.py to copy additional content

--- src/main.py
+++ src/main.py
@@ -18,12 +18,19 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    print("Generating pages from content...")
+    for root, dirs, files in os.walk(dir_path_content):
+        for file in files:
+            if file == "index.md":
+                from_path = os.path.join(root, file)
+                # Create a matching output path under /public
+                relative_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dir_path_public, relative_path)
+                dest_path = dest_path.replace(".md", ".html")
+
+                generate_page(from_path, template_path, dest_path)
+
+    print("Done.")
 
 
 main()
