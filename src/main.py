508d3d51f88121782d2c459a38e32ef8e20c6e9d
GrahamC7 <scocker@wgu.edu>
2025-06-08 13:59:24 -0400

    updating main.py to build site in docs dir instead of public dir

--- src/main.py
+++ src/main.py
@@ -6,25 +6,19 @@ from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    base_path = "/"
-    if len(sys.argv) > 1:
-        base_path = sys.argv[1]
-        if not base_path.startswith("/"):
-            base_path = "/" + base_path
-        if not base_path.endswith("/"):
-            base_path += "/"
+    base_path = sys.argv[1] if len(sys.argv) > 1 else "/"
 
-    print("Deleting public directory...")
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating pages from content...")
