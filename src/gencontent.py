fba20347909ebb98df497b5cde7e30ef1a988125
GrahamC7 <scocker@wgu.edu>
2025-06-08 13:33:50 -0400

    gencontent.py typo fix

--- src/gencontent.py
+++ src/gencontent.py
@@ -45,7 +45,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             html_path = os.path.join(dest_dir_path, relative_path)
             html_path = html_path.replace(".md", ".html")
 
-            os.markedirs(os.path.dirname(html_path), exist_ok=True)
+            os.makedirs(os.path.dirname(html_path), exist_ok=True)
 
             print(f" * {md_path} {template_path} -> {html_path}")
             generate_page(md_path, template_path, html_path)
